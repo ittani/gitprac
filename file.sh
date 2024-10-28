27bfb79c68abd2f1bf274e38154f827a052232b4
Ittani Maemo <ittanim@gmail.com>
2024-10-28 14:15:12 +0200

    new Update

---
+++ file.sh
@@ -0,0 +1,13 @@
+#This is first script ever
+#!/bin/bash
+
+echo "Please enter your name"
+read name
+echo "enter the name of your of organization"
+read company
+echo "Enter where you stay"
+read city
+echo "\n"
+echo "Here is the employee summary: \n"
+echo "Name: ${name} \nCompany: ${company} \nCity: ${city}"
+echo "\n........ The END ........"
